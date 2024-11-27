efd009a7ea8547535566fc7e446623df5d61b4b5
willrei <wnreichert@inf.ufrgs.br>
2024-11-26 21:03:26 -0300

    Implement base window

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+import constants
+
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
